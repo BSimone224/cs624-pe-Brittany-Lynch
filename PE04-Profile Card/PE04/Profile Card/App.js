da2decda078f866447c875c50d537d50754d32ad
BSimone224 <89556581+BSimone224@users.noreply.github.com>
2023-05-07 21:49:58 +0000

    Submission for PE04_Part_1 - Brittany Lynch

--- PE04-Profile Card/PE04/Profile Card/App.js
+++ PE04-Profile Card/PE04/Profile Card/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, View } from 'react-native';
+import { Image, StyleSheet, View, Text } from 'react-native';
 
 export default class App extends Component {
   render() {
@@ -10,13 +10,20 @@ export default class App extends Component {
         <Image style={styles.cardImage}
                 source={require('./user.png')}/>
       </View>
+      <Text style={styles.cardName}>{profileName}</Text>
+      <View style={styles.nameBorder}><Text style={styles.cardJob}>{profileJob}</Text>
+      </View>
+      <Text style={styles.cardProfile}>{profile}</Text>
       </View>
       </View>
     );
   }
 }
 
-const profileCardColor = 'dodgerblue';
+const profileCardColor = 'steelblue';
+const profileName = 'John Doe';
+const profileJob = 'React Native Developer'
+const profile = 'John is a really great JavaScript developer. He loves using JS to build React Native applications for iOS and Android'
 
 const styles = StyleSheet.create({
   container: {
@@ -35,16 +42,44 @@ const styles = StyleSheet.create({
     height: 400
   },
   cardImageContainer: {
-    alignItems: 'center', //new
+    alignItems: 'center',
     backgroundColor: 'white',
     borderWidth: 3,
     borderColor: 'black',
+    marginTop: 10,
     width: 120,
     height: 120,
     borderRadius: 60,
   },
   cardImage: {
+    alignItems: 'center',
+    marginTop: 15,
     width: 80,
     height: 80
+  },
+  cardName: {
+    marginTop: 25,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  nameBorder: {
+    borderBottomWidth: 2.5,
+    borderBottomColor: '#000',
+  },
+  cardJob: {
+    alignItems: 'center',
+    marginTop: 15,
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 'bold'
+  },
+  cardProfile: {
+    textAlign: 'justify',
+    marginTop: 20,
+    marginRight: 30,
+    marginLeft: 30,
+    fontSize: 15,
+    color: 'black'
   }
 });
